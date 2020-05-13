b1702297fb6c2f5be892db5cb548ef01d0caa7ab
jnolan97 <johnmnolan97@gmail.com>
2020-05-12 20:41:18 -0500

    Homework

---
+++ notes.py
@@ -0,0 +1,12 @@
+#lstrip and rstrip
+name = "   John Smith"
+print(name.lstrip())
+
+name = "Bill Ross   "
+print(name.rstrip())
+
+name = "   John Smith   "
+print(name.strip())
+
+president = "Barak obama"
+print(president.title())
